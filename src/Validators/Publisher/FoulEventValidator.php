803e7e4e5e1533a7066a355745ad0c92264d1420
Lukasz S. <dev@localhost>
2025-11-03 22:56:14 +0100

    feat: updates foul records

--- src/Validators/Publisher/FoulEventValidator.php
+++ src/Validators/Publisher/FoulEventValidator.php
@@ -12,7 +12,15 @@ class FoulEventValidator implements ValidatorInterface
     public function validate(array $data): true
     {
         if (!isset($data['match_id']) || !isset($data['team_id'])) {
-            throw new \InvalidArgumentException('match_id and team_id are required for foul events');
+            throw new \InvalidArgumentException('match_id, team_id are required for foul events');
+        }
+
+        if (!isset($data['player']) || !isset($data['affected_player'])) {
+            throw new \InvalidArgumentException('player and affected_player are required for foul events');
+        }
+
+        if (!isset($data['minute']) || !isset($data['second'])) {
+            throw new \InvalidArgumentException('Time of the foul is required for foul events');
         }
 
         return true;
